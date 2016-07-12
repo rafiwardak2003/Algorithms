7a9f14a8c8b21a91c214312edb7edaf051627938
Wardak, Rafi <Rafi.Wardak@contractor.cengage.com>
2016-07-12 17:21:51 -0400

    Initial fibonacci example.

---
+++ Algorithms/Fibonacci.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+
+namespace Algorithms
+{
+	public class Fibonacci
+	{
+		// Basic recursive example
+		public static int RecursiveFib(int position)
+		{
+			if (position < 2)
+			{
+				return position;
+			}
+			else {
+				return RecursiveFib(position - 2) + RecursiveFib(position - 1);
+			}
+		}
+
+		// Improved recursive example
+		// Cache the previous result
+		static Hashtable cachedResults = new Hashtable();
+		public static int ImprovedRecursiveFib(int position)
+		{
+			Action<int> addToCache = (result) => 
+				{
+					if (!cachedResults.ContainsKey(position))
+					{
+						cachedResults.Add(position, result);
+					}               
+				};
+
+			if (position < 2)
+			{
+				addToCache(position);
+				return position;
+			}
+			else
+			{
+				if (!cachedResults.ContainsKey(position))
+				{
+					int result = (int)cachedResults[position - 2] + ImprovedRecursiveFib(position - 1);
+					addToCache(result);
+				}
+
+				return (int)cachedResults[position];
+			}
+		}
+
+		public static int SwapFibonacci(int position)
+		{
+			int temp;
+			int a = 0;
+			int b = 1;
+			for (int i = 0; i < position; i++)
+			{
+				temp = a;
+				a = b;
+				b = temp + b;
+			}
+
+			return a;
+		}
+	}
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algorithms
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string result;
+
+			Console.WriteLine("Recursive Fibonacci");
+			result = string.Join(", ", RunFibonacci(Fibonacci.RecursiveFib));
+			Console.WriteLine(result);
+
+
+			Console.WriteLine("Improved Recursive Fibonacci");
+			result = string.Join(", ", RunFibonacci(Fibonacci.ImprovedRecursiveFib));
+			Console.WriteLine(result);
+
+			Console.WriteLine("Swap Fibonacci");
+			result = string.Join(", ", RunFibonacci(Fibonacci.SwapFibonacci));
+			Console.WriteLine(result);
+		}
+
+		static int[] RunFibonacci(Func<int, int> fibFunc)
+		{
+			int[] results = new int[10];
+			for (int i = 0; i < 10; i++)
+			{
+				results[i] = fibFunc(i);
+			}
+
+			return results;
+		}
+	}
+}
